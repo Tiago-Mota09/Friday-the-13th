9013e5ab611d55fd3f01cd5f33677fa6d893ac2d
Tiago Mota <tiagodiggermota@hotmail.com>
2020-11-13 17:45:58 -0300

    first commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <Menu>
+      <a id="menu" onclick="openMenu()">
+        <span class="material-icons">menu</span>
+      </a>
+    </Menu>
+  </div>
+</template>
+
+<style scoped>
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.material-icons {
+  color: white;
+  cursor: pointer;
+}
+
+/* .bm-burger-button {
+  position: fixed;
+  width: 36px;
+  height: 30px;
+  left: 36px;
+  top: 36px;
+  cursor: pointer;
+}
+.bm-burger-bars {
+  background-color: #373a47;
+}
+.line-style {
+  position: absolute;
+  height: 20%;
+  left: 0;
+  right: 0;
+}
+.cross-style {
+  position: absolute;
+  top: 12px;
+  right: 2px;
+  cursor: pointer;
+}
+.bm-cross {
+  background: #bdc3c7;
+}
+.bm-cross-button {
+  height: 24px;
+  width: 24px;
+}
+.bm-menu {
+  height: 100%; 
+  width: 0; 
+  position: fixed;
+  z-index: 1000; 
+  top: 0;
+  left: 0;
+  background-color: rgb(63, 63, 65);
+  overflow-x: hidden;
+  padding-top: 60px; 
+  transition: 0.5s; 
+}
+.bm-overlay {
+  background: rgba(0, 0, 0, 0.3);
+}
+.bm-item-list {
+  color: #b8b7ad;
+  margin-left: 10%;
+  font-size: 20px;
+}
+.bm-item-list > * {
+  display: flex;
+  text-decoration: none;
+  padding: 0.7em;
+}
+.bm-item-list > * > span {
+  margin-left: 10px;
+  font-weight: 700;
+  color: white;
+} */
+</style>
+
+<script>
+export default {
+  data: () => ({
+    openMenu: false,
+  }),
+  props: {
+    menu: Boolean,
+  },
+  mounted() {
+    this.openMenu = this.Menu;
+  },
+  watch: {
+    menu() {
+      this.openMenu = this.openMenu;
+    },
+  },
+  methods: {
+    closeMenu() {
+      this.opnenMenu = false;
+      this.$emit(`closed`);
+    },
+  },
+};
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,204 @@
+<template>
+
+  <div>
+
+    <header>
+      <div class="barra">
+        <Menu @click="openMenu"/>
+      <h1> Hoje é Sexta-Feira 13!</h1> 
+        <img src="../assets/calendar.png" alt="" class="calendar"/>
+      </div>
+    </header>
+
+    <div class="principal">
+      
+      <img
+        src="../assets/mask.png"
+        alt=""
+        width="50px"
+        class="mask"
+      />
+      <img 
+      class="friday"
+        src="../assets/friday.png"
+        alt=""
+      />&#x1F52A
+      
+    </div>
+    
+    <div class="content">
+      
+      <img
+        src="../assets/foto1.jpg"
+        alt="Jason foto 1"
+        class="img"
+      />
+       <i class="material-icons">favorite share</i>
+
+      <img
+        src="../assets/foto2.jpg"
+        alt="Jason foto 2"
+         class="img"
+      />
+      <i class="material-icons">favorite share</i>
+
+      <img
+        src="../assets/foto3.jpg"
+        alt="Jason foto 3"
+         class="img"
+      />
+     <i class="material-icons">favorite share</i>
+
+      <img
+        src="../assets/foto4.jpg"
+        alt="Jason foto 4"
+         class="img"
+      />
+      <i class="material-icons">favorite share</i>
+<!-- ==================================================================================== -->
+
+      <img
+        src="../assets/foto5.jpg"
+        alt="Jason foto 5"
+         class="img"
+         width="350px"
+      />
+      <i class="material-icons">favorite share</i>
+
+      <img
+        src="../assets/foto6.jpg"
+        alt="Jason foto 6"
+         class="img"
+         width="350px"
+      />
+      <i class="material-icons">favorite share</i>
+
+      <img
+        src="../assets/foto7.jpg"
+        alt="Jason foto 7"
+         class="img"
+         width="350px"
+      />
+      <i class="material-icons">favorite share</i>
+
+      <img 
+      src="../assets/foto8.jpg" 
+      alt="Halloween" 
+       class="img"
+       width="350px"
+      />
+      <i class="material-icons">favorite share</i>
+<!-- ================================================================================== -->
+      <img 
+      src="../assets/gif.gif"
+       alt="Halloween" 
+       class="gif"
+       />
+      <i class="material-icons">favorite share</i>
+
+      <img 
+      src="../assets/mask.jpg" 
+      alt="Halloween" 
+      class="gifmask"
+      />
+      <i class="material-icons">favorite share</i>
+    </div>
+  </div>
+</template>
+
+
+<style>
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.barra {
+  height: 100px;
+  width: 98.8%;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande","Lucida Sans", Arial, sans-serif;
+  color: white;
+  font-size: 20px;
+  background: rgb(117, 29, 29);
+  box-shadow: 0 10px 5px rgb(51, 35, 35);
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin: 20px 10px 30px 70px;
+}
+
+/* ====================================================================== */
+
+.calendar{
+  box-shadow: none;
+  padding-top: 30px;
+}
+.mask{
+  box-shadow: none;
+  padding-top: 30px;
+}
+
+/* ====================================================================== */
+
+body {
+  display: flex;
+  background-color: rgb(110, 70, 70);
+  padding-bottom: 100px;
+  align-items: center;
+}
+.principal {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 40px;
+}
+.friday{
+  
+  width: 250px;
+  box-shadow: none;
+  margin: 0;
+}
+
+img {
+  max-width: 775px;
+  height: auto;
+  border-radius: 10px;
+  box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.849);
+  margin: 0 10px 30px 10px;
+}
+.material-icons{
+  color: rgb(160, 120, 120);
+}
+.gif{
+width: 800px;
+height: 440px;
+}
+.gifmask{
+  width: 800px;
+  height: 440px;
+}
+
+</style>
+
+<script>
+import Menu from '@/components/Menu'
+export default {
+    data:() => ({
+      Menu: false
+    }),
+    components: {
+        Menu 
+    },
+    openMenu(){
+      this.Menu = true
+    },
+    resetMenu(){
+      this.Menu = false
+    },
+}
+
+</script>
